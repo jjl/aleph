2bf8fa96fb9049d22e5b6d05613051ae0a25880a
James Laver <james.laver@gmail.com>
2016-02-10 23:37:15 +0100

    Initial stab at it. Where are the codecs kept?

--- src/aleph/http/multipart.clj
+++ src/aleph/http/multipart.clj
@@ -6,10 +6,22 @@
      Locale]
     [java.io
      File]
+    [java.nio
+     ByteBuffer Charset]
     [java.net
      URLConnection]
     [io.netty.util.internal
-     ThreadLocalRandom]))
+     ThreadLocalRandom]
+    [io.netty.handler.codec.http.multipart
+     ])
+  (:require-clojure :as cc))
+
+;; form data and files are encoded differently
+(defn- mime-encode [val])
+(defn- base64-encode [val])
+(def encoders
+  {:base64 base64-encode
+   :quoted-printable mime-encode})
 
 (defn boundary []
   (-> (ThreadLocalRandom/current) .nextLong Long/toHexString .toLowerCase))
@@ -21,16 +33,56 @@
     (when encoding
       (str ";charset=" encoding))))
 
-(defn populate-part
-  [{:keys [name content mime-type encoding]}]
-  (if (instance? File content)
-    {:name name
-     :content (bs/to-byte-buffer content)
-     :mime-type (mime-type-descriptor
-                  (or mime-type (URLConnection/guessContentTypeFromName (.getName ^File content))))}
+(defn- make-part
+  "[internal] Generates a part map of the appropriate format"
+  [{:keys [name content mime-type charset transfer-encoding] :or {:transfer-encoding :quoted-printable}}]
+  (let [mt (or mime-type
+               (when (instance? File content)
+                 (URLConnection/guessContentTypeFromName (.getName ^File content))))]
+    {:name name :content (bs/to-byte-buffer content)
+     :mime-type (mime-type-descriptor mt charset)
+     :transfer-encoding transfer-encoding}))
 
-    ))
+(defn- part-headers [name mime-type transfer-encoding]
+  (let [te (cc/name transfer-encoding)
+        cd (str "content-disposition: form-data; name=\"" (mime-encode name) \newline)
+        ct (str "content-type: " mime-type \newline)
+        cte (str "content-transfer-encoding: " te "\n\n")
+        lcd (.length cd)
+        lct (.length ct)
+        lcte (.length cte)
+        size (+ lcd lct lcte)
+        buf (ByteBuffer/allocate size)]
+    (doto buf
+      (.put 0 (bs/to-byte-buffer cd))
+      (.put lcd (bs/to-byte-buffer ct))
+      (.put (+ lcd lct) (bs/to-byte-buffer cte)))))
 
+(defn- prepare-part
+  "[internal] Generates the byte representation of a part for the bytebuffer"
+  [{:keys [name content mime-type charset transfer-encoding] :as part}]
+  ;; encode name, content`
+  (let [encoder (get (encoders transfer-encoding) mime-encode)
+        headers (part-headers name mime-type transfer-encoding)
+        body (encoder content)
+        header-len (.length headers)
+        size (+ header-len (.length body))
+        buf (ByteBuffer/allocate size)]
+    (doto buf
+      (.put 0 headers)
+      (.put header-len body))))
 
 (defn multipart-body [boundary parts]
-  )
+  (let [b (bs/to-byte-buffer boundary)
+        b-len (+ 2 (.length boundary))
+        ps (map (comp make-part prepare-part) parts)
+        boundaries-size (* (inc (count parts)) b-len)
+        part-size (reduce (fn [acc p] (+ acc (.length p))) 0 ps)
+        buf (ByteBuffer/allocate (+ boundaries-size part-size))]
+    (.put buf 0 b)
+    (reduce (fn [idx part]
+              (let [p-len (.length part)]
+                (.put buf idx part)
+                (.put buf (+ idx part-len) b)
+                (+ idx part-len b-len))) b-len ps)
+    buf))
